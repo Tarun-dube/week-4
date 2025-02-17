3bf655c83ea0e5a3bfeff010bbe0d20f564282fb
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-17 16:00:31 +0530

    tarun add. exception propogation

---
+++ src/main/java/ExceptionPropogation/ExceptionPropagationExample.java
@@ -0,0 +1,26 @@
+package ExceptionPropogation;
+
+
+public class ExceptionPropagationExample {
+
+    // Method that throws an ArithmeticException
+    public static void method1() {
+        int result = 10 / 0; // This will throw ArithmeticException
+    }
+
+    // Method that calls method1()
+    public static void method2() {
+        method1();
+    }
+
+    // Main method that calls method2() and handles the exception
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
+
+
